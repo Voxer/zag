2056907a203ff24439f1bb9821bce23fbeef6202
sentientwaffle <sentientwaffle@gmail.com>
2014-02-07 10:22:27 -0800

    pg: fix error handling

--- backend-pg/index.js
+++ backend-pg/index.js
@@ -149,8 +149,8 @@ PostgresBackend.prototype.savePoints = function(points) {
     + "WHERE NOT EXISTS "
     + "( SELECT 1 FROM " + tKeys + " "
     +   "WHERE metrics_key=$3 )",
-    [mkey, type, mkey], this.onKeyInsert)
-    this.savePoint(mkey, pt, this.onKeyInsert)
+    [mkey, type, mkey], this._onKeyInsert)
+    this.savePoint(mkey, pt, this._onKeyInsert)
   }
 }
 
